098be52bb6bd8af4cab4480fa3df677b37ab3758
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-02-11 11:28:22 +0200

    update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,25 +1,9 @@
-const Discord = require("discord.js");
-const db = require("quick.db");
-
-module.exports.run = async (bot, message, args, utils) => {
-  if(!message.content.startsWith('m!'))return;  
-
-  let user = message.mentions.members.first() || message.author;
-
-  let bal = db.fetch(`money_${message.guild.id}_${user.id}`)
-
-  if (bal === null) bal = 0;
-
-  let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`)
-  if (bank === null) bank = 0;
-
-  let moneyEmbed = new Discord.RichEmbed()
-  .setColor("#FFFFFF")
-  .setDescription(`**${user}'s Balance**\n\nPocket: ${bal}\nBank: ${bank}`);
-  message.channel.send(moneyEmbed)
-};
-
-module.exports.help = {
-  name:"balance",
-  aliases: ["bal"]
+module.exports.run = async(client, message, args) => {
+   let ping = new Date().getTime()
+   let APIping = client.ws.ping
+   message.channel.send(`Commands reply: \`${ping}\`\nWebSocket ping: \`${APIping}\``)
+}
+module.exports.config = {
+    name: "ping",
+    aliases: ['']
 }
