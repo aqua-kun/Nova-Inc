f12d9464ddfe6eaeb183b4eed0258eb829efd649
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-02-11 21:11:29 +0200

    delete ping.js

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async(client, message, args) => {
-   let ping = new Date().getTime()
-   let APIping = client.ws.ping
-   message.channel.send(`Commands reply: \`${ping}\`\nWebSocket ping: \`${APIping}\``)
-}
-module.exports.config = {
-    name: "ping",
-    aliases: ['']
-}
