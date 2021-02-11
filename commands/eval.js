99ed319556f7462f3ea191377d3229df8c70b658
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-02-11 11:40:15 +0200

    update eval.js

---
+++ commands/eval.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js")
+const fs = require("fs")
+const { inspect } = require('util')
+module.exports.run = async (client, message, args) => {
+const ownerID = '736174044499673140'
+
+    if(args.join(' ').endsWith('-js')) {
+    if(message.author.id == ownerID) {           
+                try {
+let argss = args.join(' ')
+let evaled =  await eval(argss);
+let eevaled = typeof evaled;
+const tyype = eevaled[0].toUpperCase() + eevaled.slice(1);
+if(typeof evaled!== 'string') evaled = require ('util').inspect(evaled, { depth: 0});
+
+evaled == (undefined || null) ? evaled = 'Пустой запрос: ' + evaled : evaled;
+const ping = new Date().getTime() - message.createdTimestamp;
+const embed = new Discord.MessageEmbed()
+.addField('Вывод:', `\`\`\`js\n${evaled}\n\`\`\`\n\`\`\`js\nType: ${typeof evaled}\nВыполнено за: ${ping}ms\n\`\`\``)
+.setTimestamp()
+message.channel.send(embed)
+
+} catch (e) {
+const ping = new Date().getTime() - message.createdTimestamp;
+const embed = new Discord.MessageEmbed()
+.addField('Вывод:', `\`\`\`js\n❌${e}\n\`\`\`\n\`\`\`js\nType: ${typeof evaled}\nВыполнено за: ${ping}ms\n\`\`\``)
+.setTimestamp()
+message.channel.send(embed)
+    
+    }
+            
+        }
+    }
+}
+module.exports.help = {
+  name: "eval",
+  aliases: ['e']
+}
