fb4c34063ae6e26fe1583158e53dfb6524061cf5
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-02-11 11:19:18 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const botconfig = require("./botconfig.json");
+const clientconfig = require("./clientconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
+const client = new Discord.Client({disableEveryone: true});
 
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 
 
 fs.readdir("./commands/", (err, files) => {
@@ -20,42 +20,42 @@ fs.readdir("./commands/", (err, files) => {
   jsfile.forEach((f, i) =>{
     let props = require(`./commands/${f}`);
     console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-    props.help.aliases.forEach(alias => { 
-      bot.aliases.set(alias, props.help.name);
+    client.commands.set(props.config.name, props);
+    props.config.aliases.forEach(alias => { 
+      client.aliases.set(alias, props.config.name);
   
   });
 });
 })
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity(`In Development`);
-  bot.user.setStatus('online');
+client.on("ready", async () => {
+  console.log(`${client.user.username} is online on ${client.guilds.size} servers!`);
+  client.user.setActivity(`In Development...`);
+  client.user.setStatus('dnd');
 
-  bot.on("message", async message => {
-    if(message.author.bot) return;
+  client.on("message", async message => {
+    if(message.author.client) return;
     if(message.channel.type === "dm") return;
-    let prefix = botconfig.prefix
+    let prefix = clientconfig.prefix
     let messageArray = message.content.split(" ");
     let args = message.content.slice(prefix.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
     let commandfile;
 
-    if (bot.commands.has(cmd)) {
-      commandfile = bot.commands.get(cmd);
-  } else if (bot.aliases.has(cmd)) {
-    commandfile = bot.commands.get(bot.aliases.get(cmd));
+    if (client.commands.has(cmd)) {
+      commandfile = client.commands.get(cmd);
+  } else if (client.aliases.has(cmd)) {
+    commandfile = client.commands.get(client.aliases.get(cmd));
   }
   
       if (!message.content.startsWith(prefix)) return;
 
           
   try {
-    commandfile.run(bot, message, args);
+    commandfile.run(client, message, args);
   
   } catch (e) {
   }}
   )})
 
 
-bot.login("Discord Token");
+client.login("Discord Token");
