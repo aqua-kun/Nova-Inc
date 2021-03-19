dd45935ea5d7b1aadb2dbf77e474b9a41c29f8ec
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-03-19 17:17:47 +0200

    update tiktok-user.js

---
+++ commands/tiktok-user.js
@@ -0,0 +1,12 @@
+module.exports.run = async(client, message, args) => {
+    const argss = args.join(" ")
+    const tiktok = require('tiktok-app-api');
+
+    let tiktokApp = await tiktok();
+    const user = await tiktokApp.getUserByName(argss);
+    const userInfo = await tiktokApp.getUserInfo(user);
+    console.log(userInfo)
+
+      
+})();
+}
