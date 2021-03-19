b4c6b469a8b38115afaac466e04d631b843f0ffd
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-03-19 22:22:55 +0200

    update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -2,9 +2,9 @@ const Discord = require ('discord.js')
 var neko = require('nekos.life');
 var {sfw} = new neko();
 module.exports.run = async(client, message, args) => {
-    const user = message.mentions.users.first().id || message.author
+    const user = message.mentions.users.first() || message.author
     const emb = new Discord.MessageEmbed()
-    .setDescription(`****Кристина обняла <@${user}> <3****`.replace(`<@${message.author}>`, "тебя"))
+    .setDescription(`****Кристина обняла ${user} <3****`.replace(`${message.author}`, "тебя"))
     .setColor(0x00ff00)
     .setImage(await sfw.hug().url);
     message.channel.send(emb)
