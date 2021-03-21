1fd96f81be58d5494544dcdeaf73716ad7a20cf2
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-03-21 14:37:22 +0200

    update tiktok-user.js

--- commands/tiktok-user.js
+++ commands/tiktok-user.js
@@ -1,9 +1,5 @@
 const tiktok = require ("tiktok-stats");
 module.exports.run = async(client, message, args) => {
-    var length = args[0].length
-    var argss = args.join(/ +/g);
-    argss = argss.replace(/ +/g, "");
-    argss = argss.slice(length)
     const stats = await new tiktok.Stats("fouand").getStats().then(s => {
     const emb = new Discord.MessageEmbed()
     .setImage(s.user.avatar)
