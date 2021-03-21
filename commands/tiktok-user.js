d8a7bc6c73578d8489ab668148689ab7f0350428
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-03-21 14:06:05 +0200

    update tiktok-user.js

--- commands/tiktok-user.js
+++ commands/tiktok-user.js
@@ -1,6 +1,9 @@
 const tiktok = require ("tiktok-stats");
 module.exports.run = async(client, message, args) => {
-    const argss = args.join(" ");
+    var length = args[0].length
+    var argss = args.join(/ +/g);
+    argss = argss.replace(/ +/g, "");
+    argss = argss.slice(length)
     const stats = await new tiktok.Stats(argss).getStats();
     console.log(stats)
 }
