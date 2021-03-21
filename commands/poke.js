b99509ad0a372555ab1edadd22106f74d797446d
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-03-21 22:26:14 +0200

    update poke.js

---
+++ commands/poke.js
@@ -0,0 +1,16 @@
+const Discord = require ('discord.js')
+var neko = require('nekos.life');
+var {sfw} = new neko();
+module.exports.run = async(client, message, args) => {
+    const user = message.mentions.users.first() || message.author
+    const emb = new Discord.MessageEmbed()
+    .setDescription(`****Кристина ткнула ${user} <3****`.replace(`${message.author}`, "тебя"))
+    .setColor(0x00ff00)
+    .setImage(await sfw.poke().then(u => u.url));
+    message.channel.send(emb)
+    message.channel.send(await sfw.hug())
+}
+module.exports.config = {
+    name: "hug",
+    aliases: [""]
+}
