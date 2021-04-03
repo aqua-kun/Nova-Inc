272d5c2b5640dfb09c0494c7363b6a09d4904d04
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-04-03 17:09:17 +0300

    update smug.js

---
+++ commands/smug.js
@@ -0,0 +1,15 @@
+const Discord = require ('discord.js')
+var neko = require('nekos.life');
+var {sfw} = new neko();
+module.exports.run = async(client, message, args) => {
+    const user = message.mentions.users.first() || message.author
+    const emb = new Discord.MessageEmbed()
+    .setDescription(`****${message.author.mention} выглядит самодовольно <3****`)
+    .setColor(0x00ff00)
+    .setImage(await sfw.smug().then(u => u.url));
+    message.channel.send(emb)
+}
+module.exports.config = {
+    name: "poke",
+    aliases: [""]
+}
