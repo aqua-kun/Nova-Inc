61e25113004c0f218d7a2112af0823bff153d2cd
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-04-04 11:41:37 +0300

    update baka.js

--- commands/baka.js
+++ commands/baka.js
@@ -3,12 +3,14 @@ var neko = require('nekos.life');
 var {sfw} = new neko();
 module.exports.run = async(client, message, args) => {
     const user = message.mentions.users.first() == message.author ? message.channel.send('ты не дурак <3') : message.mentions.users.first() || message.author
-    message.mentions.users.first() == message.author ? return : const aboba = message.mentions.users.first()
+    message.mentions.users.first() == message.author ? '' : myfunc()
+    function myfunc() {
     const emb = new Discord.MessageEmbed()
     .setDescription(`****${message.author} обозвал дураком ${user}<3****`).replace(message.author, 'кого-то')
     .setColor(0x00ff00)
     .setImage(await sfw.baka().then(u => u.url));
     message.channel.send(emb)
+    }
 }
 module.exports.config = {
     name: "baka",
