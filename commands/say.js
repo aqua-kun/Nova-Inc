1a68c1aaeda2d19134b5f57e7d0bd099046ca37b
aqua-kun <78872855+aqua-kun@users.noreply.github.com>
2021-04-08 19:19:38 +0300

    update say.js

---
+++ commands/say.js
@@ -0,0 +1,17 @@
+module.exports.run = async(client, message, args) => {
+    const argss = message.content.slice(6).split(";")
+    try {
+        if(message.member.hasPermission("ADMINISTRATOR")) {
+            const embed = new Discord.MessageEmbed()
+            .setTitle(argss[0])
+            .setDescription(argss[1])
+            .setFooter(argss[2])
+            .setColor(argss[3]);
+            message.channel.send(embed)
+        } message.channel.send("У вас нет прав!")
+    } catch(e) message.channel.send("Команда использована не правильно! Попробуйте так: >>say title;description;footer;color")
+}
+module.exports.config = {
+    name: "say",
+    aliases: [""]
+}
